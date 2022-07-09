e3217e9166601e24a42ec6b6b49961beb84b022b
Ana Carolinne <38358019+AnaCarolinneS@users.noreply.github.com>
2022-07-09 18:25:55 -0300

    update
    update

--- dist/app.ts
+++
@@ -1,6 +0,0 @@
-import { Negociacao } from "../app/models/negociacao";
-
-const negociacao = new Negociacao(new Date(), 10, 100); //data, quantidade, 100
-console.log(negociacao.volume);
-alert('oi');
-
