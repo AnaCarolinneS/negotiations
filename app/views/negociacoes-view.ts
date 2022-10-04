7dc9f39a7bd2e04debba042dc5b15894ab8d3b5f
Ana Carolinne <38358019+AnaCarolinneS@users.noreply.github.com>
2022-10-04 20:54:15 -0300

    Unindo view e modelo
    Unindo view e modelo para exibir no console

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,3 +1,5 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
 export class NegociacoesView {
     private elemento: HTMLElement;
 
@@ -6,7 +8,7 @@ export class NegociacoesView {
     };
 
 
-    template(): string { //metodo template - declarar o template da minha view - tem que ser metodo
+    template(model: Negociacoes): string { //metodo template - declarar o template da minha view - tem que ser metodo
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -17,14 +19,30 @@ export class NegociacoesView {
                 </tr>
             </thead>
             <tbody>
+            <tr>
+            ${model.lista().map(negociacao => {
+                return `
+                <tr>
+                <td>${negociacao.data}</td>
+                <td>${negociacao.quantidade}</td>
+                </tr>
+                `;
+
+            }).join('')}  
+            </tr>
             </tbody>
         </table>
 
         `;
     }
-    update(): void {
-        this.elemento.innerHTML = this.template();
+    update(model: Negociacoes): void {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
 
+   //para cada item da lista, ira fazer um map, pq o resultado tem que ser convertido numa string, para ser inserida no tbody. model.list().map
+
+   //depois de pegar minha lista convertida em um array, onde cada elemento é uma string, torna todo mundo em uma stringzona, com o separador sendo um ''.
     //metodo update - para renderizar o template, através do elmeento que eu capturei, atraves do construtor.
 }
