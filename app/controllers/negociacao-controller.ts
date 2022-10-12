64a97c4fd30547248a2bc5da19a00cecf1a0d0ce
Ana Carolinne <38358019+AnaCarolinneS@users.noreply.github.com>
2022-10-12 09:57:22 -0300

    View

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,5 +1,6 @@
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
 export class NegociacaoController {
@@ -8,7 +9,7 @@ export class NegociacaoController {
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView'); //seletor sendo chamado no index, abaixo do form.
-
+    private mensagemView = new MensagemView('#mensagemView');
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
